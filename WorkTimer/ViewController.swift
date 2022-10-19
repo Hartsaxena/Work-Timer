b7b23eb02c8f64ec76a063b7dc18e277a5390225
Hartsaxena <64151017+Hartsaxena@users.noreply.github.com>
2022-10-19 09:54:00 -0500

    Initial Commit

---
+++ WorkTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkTimer
+//
+//  Created by James Sun on 10/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
